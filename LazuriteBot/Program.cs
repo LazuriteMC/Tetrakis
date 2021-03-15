04bc51f1ef324d7d5827aacc25362691dabd7268
ethanejohnsons <ethan@ejohnsons.com>
2021-03-15 15:19:09 -0500

    Initial code commit

---
+++ LazuriteBot/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices.ComTypes;
+using System.Threading.Tasks;
+using DSharpPlus;
+using Microsoft.Extensions.Logging;
+
+namespace LazuriteBot
+{
+    class Program
+    {
+        private static string token;
+        
+        static void Main(string[] args)
+        {
+            token = File.ReadAllText("token.txt");
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync()
+        {
+            var discord = new DiscordClient(new DiscordConfiguration()
+            {
+                Token = token,
+                TokenType = TokenType.Bot,
+                MinimumLogLevel = LogLevel.Debug
+            });
+            
+            discord.MessageCreated += async (s, e) =>
+            {
+                if (e.Message.Content.ToLower().StartsWith("ping"))
+                {
+                    await e.Message.RespondAsync("pong!");
+                }
+            };
+
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
+        }
+    }
+}
